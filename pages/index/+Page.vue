9f40681d73a5fc4cfdccccefc4da4daa977216d5
chris <chris.truelson@noisedestroyer.com>
2024-01-15 05:03:35 +0800

    work

--- pages/index/+Page.vue
+++ pages/index/+Page.vue
@@ -1,12 +1,33 @@
 <template>
-  <h1>Welcome</h1>
-  This page is:
-  <ul>
-    <li>Rendered to HTML.</li>
-    <li>Interactive. <Counter /></li>
-  </ul>
+    <div class="home">
+        <h1>The Easiest Course to Learn AI</h1>
+        <div>Take 3 hours, and you'll be an intermediate level AI developer</div>
+        <div>Don't watch other courses on YouTube that don't go over the details. This course is all you need to become an expert!</div>
+        <div>Other courses skip the important information that you have to know to be useful. They teach old, repetitive ways of using frameworks, without actually explaining how any of it works. This course is different. It teaches everything you need to know</div>
+        <h4>When you are done with this class, the only thing left to make you an expert will be practice.</h4>
+        <div>You will have a full understanding of how all AI works (in the modern day)</div>
+        <ul>
+            <li>You will learn the science of backpropagation (how networks actually learn). This is uncommon in many courses. Here you will get to learn it all.</li>
+            <li>You will learn how to use Pytorch, the most important framework right now (OpenAI uses it)</li>
+            <li>Most importantly, you will understand <i>why</i> Pytorch works as good as it does. You won't have to guess what it's doing - you will have a great idea of how backprop works, so you can customize it if you need to.</li>
+            <li>You will become excellet at using Numpy, which is very similar to Pytorch, but some people don't know as much about Pytorch.</li>
+            <li>You will have fun. These videos make learning more fun than anything you've ever learned. Not because they are "just awesome". It's because they are so dense, you will be done before you are bored, and have insanely valuable skills. <b>These videos are worth tens of thousands of dollars. After watching them you will be able to command a high salary just because you watched them (if you had reasonably good understanding, that is).</b></li>
+        </ul>
+        <h3>How to Learn AI In The Modern Day</h3>
+        <div>AI is all the rage, but most people don't know how to start learning it.  You could spend 6 months searching YouTube and other sites but still never learn what you need to know.  You see YouTube is designed by content creators to keep you coming back. This course is the opposite.  It's designed so you never have to come back (or if you do, you can just rewatch the same video - which is a good idea!)  The bottom line is this course is designed to be everything you need to get good at AI.  It's insanely high value, and is just 3 or 4 hours long so you don't have to waste your time. <b><i>I worked hard to get this video series down to just 3 hours so you can have fun and become an expert faster than anybody you have ever met.  Don't be decieved by courses that offer 500 hours of content.  If it was useful content, it wouldn't be so cheap!  This is 3 hours of the most important content on planet earth right now, perfectly condensed so it's easy to understand.  This is the best $50 you'll ever spend!</i></b></div>
+        <div>For just $1 you can get the first course.  It's designed to be so simple anyone, even someone with NO COMPUTER EXPERIENCE can understand.  It explains how AI works so that when you do the next two videos, they will be much easier.  AI is not easy.  It's taken thousands of years for us to figure out how AI works.  But with it, the world is changing.  MAKE SURE TO PAY CLOSE ATTENTION TO THE FIRST VIDEO BECAUSE EVERYTHING IN IT IS NECESSARY FOR THE SECOND!</div>
+        <h4>How To Learn AI</h4>
+        <div>The process of learning AI is very complex - unless you have a good teacher. <b><i>This course is designed to take you from beginner knowing nothing to intermediate level in just three hours!</i></b> You'll need to rewatch the videos, most likely, 2 weeks after the first time, or later.  Once you do that, you will internalize the content extremely well and will be capable <i><b>Of making your own neural networks completely from scratch without any help at all!</b></i>  This skill is so valuable employers will pay top dollar to have you help them. But more importantly, <b><i>You will be able to make your own AI company if you can figure out how to get funding once you have a prototype ready.</i></b> </div>
+        <div>Learning AI is so hard, so there are three quick steps that this video series does to make it simple.</div>
+        <ol>
+            <li>Understand 100% how the brain works, to the best of our ability.  In course 1, we study how Geoffrey Hinton, and other AI pioneers interpret the brain, and how it is the brain can be simulated from a conceptual perspective. <i><b>This is the most important video because it explains the 'why' - it will be useful for the rest of your life even if you don't know anything about computers or programming.  But if you do, it's necessary for the next two.</b></i></li>
+            <li>Video #2 is designed to teach you the fundamentals of AI by building everything from scratch.  In this video you will see everything from Video #1 put into practice.  This is so valuable and fun, but so hard to find on the internet.  This is the most important video if you are a developer, after you watch Video #2.  However, even though this video is not as important as Video #1, <b><i>It is much more fun because you get to make the computer actually do something for you. 😀😀</i></b></li>
+            <li>Video #3 is the best video because you will watch a brand new, never-before-seen neural network take place before your eyes.  You will get to watch the creation of a new intelligence - like watching a baby be born!  The only difference is it's not gross and there's no lady involved 😀. Seriously though, this video will teach how to use CUDA (Nvidia hardware and software if you have it), Pytorch, and all the modern tools while a brilliant AI is built.  You will learn everything you need to know to start building your own AIs!😀  <b><i>This is the most fun because after you are done, you can now go and build a billion dollar company (if you know how to do funding 😀)</i></b>. These videos will also help you help others and you should be able to charge at least $500 per hour for helping people if you have reasonably good soft skills and practice a bit.  Good luck!</li>
+        </ol>
+        <div>With the skills you learn in these videos, you will be so valuable to the world <i>money cannot adequately describe how valuable you will be!</i>  People don't get this, but money doesn't buy living forever.  No amount of money can get you there <i>because nobody understands DNA good enough.</i>  Money can only buy what someone else can give, and if nobody has the tech to fix DNA problems <i>everybody dies!</i>  With this tech, the hope is that starving kids in Africa will be fed by machines that farm for them.  And the goal is to have AI do medical procedures no human can, with accuracy <i><b>to save lives!</b></i>  Because of this, <b><i>If you can't afford this video series, just buy the first one. If you can't afford the others, make sure to contact me via email and I will make sure you get it at a price you can afford.  We all have to work together, and I want to help. But have to pay the bills 😄😄</i></b></div>
+    </div>
 </template>
 
 <script lang="ts" setup>
-import Counter from './Counter.vue'
+
 </script>
--- renderer/PageShell.vue
+++ renderer/PageShell.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="layout">
-    <div class="navigation">
-      <a href="/" class="logo">
-        <img src="./logo.svg" height="64" width="64" alt="logo" />
-      </a>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+    <div class="layout">
+        <div class="navigation">
+            Nav
+        </div>
+        <div class="content">
+            <slot />
+        </div>
     </div>
-    <div class="content"><slot /></div>
-  </div>
 </template>
 
 <script lang="ts" setup>
@@ -17,39 +15,37 @@ import Link from './Link.vue'
 
 <style>
 body {
-  margin: 0;
-  font-family: sans-serif;
+    margin: 0;
+    font-family: sans-serif;
 }
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 a {
-  text-decoration: none;
+    text-decoration: none;
 }
 </style>
 
 <style scoped>
 .layout {
-  display: flex;
-  max-width: 900px;
-  margin: auto;
+    display: flex;
+    flex-direction: column;
 }
 .content {
-  padding: 20px;
-  border-left: 2px solid #eee;
-  padding-bottom: 50px;
-  min-height: 100vh;
+    padding: 20px;
+    border-left: 2px solid #eee;
+    padding-bottom: 50px;
 }
 .navigation {
-  padding: 20px;
-  flex-shrink: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  line-height: 1.8em;
+    padding: 20px;
+    flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    line-height: 1.8em;
 }
 .logo {
-  margin-top: 20px;
-  margin-bottom: 10px;
+    margin-top: 20px;
+    margin-bottom: 10px;
 }
 </style>
